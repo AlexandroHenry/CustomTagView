24016842e68fadd9e353ccd66807097f4c80a6c1
alaxhenry0121 <alaxhenry0121@gmail.com>
2023-02-26 00:34:35 +0900

    Initial Commit

---
+++ CustomTagView/CustomTagViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTagViewApp.swift
+//  CustomTagView
+//
+//  Created by Seungchul Ha on 2023/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CustomTagViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
